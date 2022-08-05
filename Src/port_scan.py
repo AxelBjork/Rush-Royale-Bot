c04ecee7cb8e3ba063d4a1459a6aab738b874624
AxelBjork <axebjr-6@student.ltu.se>
2022-08-05 19:07:17 +0200

    Cleaned up scrcpy path

--- Src/port_scan.py
+++ Src/port_scan.py
@@ -5,24 +5,23 @@ import threading
 import time
 import os
 from subprocess import check_output,Popen
-import configparser
 
 # Connects to a target IP and port, if port is open try to connect adb
-def connect_port (scrcpy_path,ip,port,batch,open_ports):
+def connect_port (ip,port,batch,open_ports):
     for tar_port in range(port, port+batch):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex((ip,tar_port))
         if result == 0:
             open_ports[tar_port]='open'
             # Make it Popen and kill shell after couple seconds
-            p = Popen(f'{os.path.join(scrcpy_path,"adb")} connect {ip}:{tar_port}', shell=True)
+            p = Popen(f'.scrcpy/adb connect {ip}:{tar_port}', shell=True)
             time.sleep(3) # Give real client 3 seconds to connect
             p.terminate() 
     return result == 0
 
 # Attemtps to connect to ip over every port in range
 # Returns device if found
-def scan_ports(scrcpy_path,target_ip,port_start,port_end, batch=3):
+def scan_ports(target_ip,port_start,port_end, batch=3):
     threads = []
     open_ports = {}
     port_range = range(port_start, port_end,batch)
@@ -30,7 +29,7 @@ def scan_ports(scrcpy_path,target_ip,port_start,port_end, batch=3):
     print(f"Scanning {target_ip} Ports {port_start} - {port_end}")
     # Create one thread per port
     for port in port_range:
-        thread = threading.Thread(target=connect_port, args=(scrcpy_path,target_ip,port,batch,open_ports))
+        thread = threading.Thread(target=connect_port, args=(target_ip,port,batch,open_ports))
         threads.append(thread)
     # Attempt to connect to every port
     for thread in threads:
@@ -42,12 +41,12 @@ def scan_ports(scrcpy_path,target_ip,port_start,port_end, batch=3):
     # Get open ports
     port_list = list(open_ports.keys())
     print(f"Ports Open: {port_list}")
-    deivce = get_adb_device(scrcpy_path)
+    deivce = get_adb_device()
     return deivce
 
 # Check if adb device is already connected 
-def get_adb_device(scrcpy_path):
-    devList = check_output(f'{os.path.join(scrcpy_path,"adb")} devices')
+def get_adb_device():
+    devList = check_output('.scrcpy/adb devices')
     devListArr = str(devList).split('\\n')
     # Check for online status
     deivce = None
@@ -57,17 +56,16 @@ def get_adb_device(scrcpy_path):
             deivce = client_ip
             print("Found ADB device! {}".format(deivce))
         else:
-            os.system(f'{os.path.join(scrcpy_path,"adb")} disconnect {client_ip}')
+            os.system(f'.scrcpy/adb disconnect {client_ip}')
     return deivce
 
 
 def get_device():
-    scrcpy_path = '.scrcpy'
-    os.system(f'{os.path.join(scrcpy_path,"adb")} devices')
+    os.system('.scrcpy/adb devices')
     # Check if adb got connected
-    device = get_adb_device(scrcpy_path)
+    device = get_adb_device()
     if not device:
         # Find valid ADB device by scanning ports
-        device=scan_ports(scrcpy_path,'127.0.0.1',48000,65000)
+        device=scan_ports('127.0.0.1',48000,65000)
     if device:
         return device
